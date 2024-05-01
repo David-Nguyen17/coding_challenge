bde9975972168e069b724fc561784cc845847404
David Nguyen <nguyenvandu199740@gmail.com>
2024-05-01 22:21:48 +0700

    feat: new width

--- components/formStep/Header.tsx
+++ components/formStep/Header.tsx
@@ -1,12 +1,22 @@
 import { CheckIcon, InfoIcon } from "@/app/assets/icons";
 import { STEP_TYPE } from "@/app/types/FormStepType";
 import Image from "next/image";
+import { useMemo } from "react";
 export interface IProps {
   activeStep: STEP_TYPE;
   percentLine: number;
 }
 const Header = (props: IProps) => {
   const { activeStep, percentLine } = props;
+  const animationLine = useMemo(() => {
+    if (activeStep === STEP_TYPE.FIRST_STEP) {
+      return "w-[10%]";
+    }
+    if (activeStep === STEP_TYPE.SECOND_STEP) {
+      return "w-[50%]";
+    }
+    return "w-[90%]";
+  }, [activeStep]);
   return (
     <div
       className={`${
@@ -18,7 +28,7 @@ const Header = (props: IProps) => {
           className={`w-[80%] lg:max-w-[834px] md:max-w-[92%] rounded-lg h-1 flex my-2`}
         >
           <div
-            className={`bg-guppies_green relative h-1 w-[${percentLine}%] 
+            className={`bg-guppies_green relative h-1 ${animationLine}
                 transition-all duration-300 ease-linear rounded-s-lg`}
           >
             <div
@@ -26,22 +36,20 @@ const Header = (props: IProps) => {
                   transition-all duration-300 ease-linear
              `}
             >
-              <div
-                className={`text-xs font-medium leading-5	text-script_ink ${
+              <span
+                className={`text-xs font-medium leading-5text-script_ink ${
                   activeStep === STEP_TYPE.THIRD_STEP
                     ? "text-white text-left -translate-x-1/2"
                     : ""
                 }`}
               >
                 {percentLine}% geschafft
-              </div>
+              </span>
               <Image src={CheckIcon} alt="Icon check" className="w-5 h-5" />
             </div>
           </div>
           <div
-            className={`bg-aurora_green h-1 flex-1 w-[${
-              100 - percentLine
-            }%] transition-all duration-300 ease-linear rounded-e-lg`}
+            className={`bg-aurora_green h-1 flex-1 transition-all duration-300 ease-linear rounded-e-lg`}
           />
         </div>
       </div>
